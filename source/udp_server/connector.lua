bd49d360c6fcfddee67d66e125e01bf261d2bc0d
Sven Hecht <sven.hecht@gmail.com>
2015-05-19 20:57:25 +0200

    some code for connector

--- source/udp_server/connector.lua
+++ source/udp_server/connector.lua
@@ -3,6 +3,31 @@ local moduleName = ...
 local M = {}
 _G[moduleName] = M
 
-function M.connect()
-	
+local wifi_timer = 3
+local wifi_check_interval = 1000
+local wifi_init_callback = nil
+
+function try_connect()
+	if wifi.sta.status() == 5 then
+		tmr.stop(wifi_timer)
+		ip = wifi.sta.getip()
+		print(ip)
+
+		wifi_init_callback()
+	else
+	    wifi.setmode(wifi.STATION)
+	    wifi.sta.config("SSID","password")
+	end
+end
+
+function set_ap()
+	cfg = {}
+	cfg.ssid = "ESP UDP Server"
+	cfg.pwd = ""
+	wifi.ap.config(cfg)
+end
+
+function M.connect(callback)
+	wifi_init_callback = callback
+	tmr.alarm(wifi_timer, wifi_check_interval, 1, try_connect)
 end
