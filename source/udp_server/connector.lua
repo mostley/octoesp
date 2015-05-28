9393e82a42c6088c34916fe026d17e9cd6e295c2
Sven Hecht <sven.hecht@gmail.com>
2015-05-28 22:17:36 +0200

    added udp server and ws2812 code

--- source/udp_server/connector.lua
+++ source/udp_server/connector.lua
@@ -6,6 +6,7 @@ _G[moduleName] = M
 local wifi_timer = 3
 local wifi_check_interval = 1000
 local wifi_init_callback = nil
+local web_server = nil
 
 function try_connect()
 	if wifi.sta.status() == 5 then
@@ -25,6 +26,16 @@ function set_ap()
 	cfg.ssid = "ESP UDP Server"
 	cfg.pwd = ""
 	wifi.ap.config(cfg)
+
+	web_server = net.createServer(net.TCP)
+    web_server:listen(80, function(conn)
+      conn:on("receive", function(conn, payload)
+      	print(payload)
+      	-- TODO find out if post and save data
+        conn:send('<h1>Welcome to the OctoESP Configurator.</h1><br/><label for="ssid">SSID:</label><form name="ssid" method="POST" action="/"><br/><label for="password">Password:</label><input name="password" type="text" value=""/><input type="password" value=""/>')
+      end)
+      conn:on("sent",function(conn) conn:close() end)
+    end)
 end
 
 function M.connect(callback)
--- source/udp_server/server.lua
+++ source/udp_server/server.lua
@@ -3,5 +3,23 @@ local moduleName = ...
 local M = {}
 _G[moduleName] = M
 
+PORT = 6803
+PIXEL_SIZE = 3
+PIXEL_PIN = 4
+
 function M.serve()
+	server = net.createServer(net.UDP)
+	server:on("receive", function(s, data)
+		print(data)
+		
+		ws2812.writergb(PIXEL_PIN, data)
+		--for i = 0,data:len() do
+		--	r = string.byte(data, i*PIXEL_SIZE + 0)
+		--	g = string.byte(data, i*PIXEL_SIZE + 1)
+		--	b = string.byte(data, i*PIXEL_SIZE + 2)
+		--	ws2812.writergb(PIXEL_PIN, string.char(r, g, b))
+		--end
+	end)
+
+	server:listen(PORT)
 end
