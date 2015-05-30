fb1246e6ff132be023f942fa5ff8fc0fc3454209
Sven Hecht <sven.hecht@gmail.com>
2015-05-31 01:43:13 +0200

    more bugfixing of udp server

--- source/udp_server/connector.lua
+++ source/udp_server/connector.lua
@@ -5,16 +5,16 @@ _G[moduleName] = M
 require('accesspoint')
 
 local wifi_timer = 3
-local wifi_check_interval = 1000
+local wifi_check_interval = 4000
 local wifi_init_callback = nil
 local web_server = nil
 local wifi_credentials = { { ["ssid"] = "matrix", ["password"] = "51whc3xt" } }
-local wifi_credential_counter = 0
+local wifi_credential_counter = 1
 local wifi_current_ssid = ""
 
 local function try_connect()
 	print("try_connect")
-	print(wifi.sta.status())
+
 	if wifi.sta.status() == 5 then
 		tmr.stop(wifi_timer)
 		ip = wifi.sta.getip()
@@ -22,9 +22,9 @@ local function try_connect()
 
 		wifi_init_callback()
 	else
-		credential_count = table.getn(wifi_credentials)
+		credential_count = table.maxn(wifi_credentials)
 		if wifi_credential_counter > credential_count or credential_count == 0 then
-			wifi_credential_counter = 0
+			wifi_credential_counter = 1
 			
 			print("all known credentials failed, starting AP")
 			tmr.stop(wifi_timer)
@@ -40,6 +40,7 @@ local function try_connect()
 
 			print("trying to connect to " .. credentials["ssid"] .. ":" .. credentials["password"])
 		    wifi.sta.config(credentials["ssid"], credentials["password"])
+		    
 		    wifi_current_ssid = credentials["ssid"]
 		    wifi_credential_counter = wifi_credential_counter + 1
 		end
